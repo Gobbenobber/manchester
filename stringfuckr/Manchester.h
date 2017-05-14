6378f7ebd0eabb2828a82a533c07882eac2e11ca
reklezz <drelixgaming@gmail.com>
2017-05-15 00:02:35 +0200

    Made a converter, the output is still sketchy a best. Haven't been able to work with the output yet.

--- stringfuckr/Manchester.h
+++ stringfuckr/Manchester.h
@@ -1,9 +1,12 @@
 #pragma once
+
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-char* stringToManchester(char* toBeConverted) {
+
+char* stringToManchester(char* toBeConverted) 
+{
 	if (toBeConverted == NULL) return 0;						// Hvis der ikke er input, return 0 
 	size_t len = strlen(toBeConverted);							// Lav size_t som kan passes til malloc.
 	char* manchester = (char *) malloc(len * 2);				// Hver char er en byte (8 bits) * 2 for manchester (>og + 1 til sidst for null terminator?<)
@@ -20,4 +23,29 @@ char* stringToManchester(char* toBeConverted) {
 		}
 	}
 	return manchester;											// Returnér manchesterkoden
+}
+
+char* mancesterToString(char* toBeConverted) {
+	if (toBeConverted == NULL) return 0;
+	int len = (strlen(toBeConverted) / 16) - 1;
+	char* tempString = (char *)malloc(len * 8);
+	char* toString = (char *)malloc(len);
+	toString[0] = '\0';
+	tempString[0] = '\0';
+
+	for (int i = 0; i <= len; i++)
+	{
+		for (int j = 0; j < 16; j += 2)
+		{
+			if ((toBeConverted[(i * 16) + j] == '0') && (toBeConverted[((i * 16) + j) + 1] == '1'))
+			{
+				strcat(tempString, "0");
+			}
+			else
+			{
+				strcat(tempString, "1");
+			}
+		}
+	}
+	return tempString;
 }
--- stringfuckr/Source.c
+++ stringfuckr/Source.c
@@ -7,9 +7,13 @@
 
 int main() {
 
-	char* str = "Tekst som skal konverteres til manchester kode.";	
+	char* str = "a";	
+	char* str2 = "0110100101011010011010011010101001101001011001010110100101100110";
 
 	printf("%s", stringToManchester(str));
+	printf("\n");
+	printf("%s", mancesterToString(str2));
+
 
 	while (1)
 	{
